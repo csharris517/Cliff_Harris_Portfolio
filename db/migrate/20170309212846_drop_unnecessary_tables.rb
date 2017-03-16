b2a7c50ba4c7e53157dcf9d95b40414da76283f4
Cliff Harris <csharris5@uh.edu>
2017-03-15 19:37:30 -0500

    removed bad migration file

--- db/migrate/20170309212846_drop_unnecessary_tables.rb
+++
@@ -1,8 +0,0 @@
-class DropUnnecessaryTables < ActiveRecord::Migration
-  def change
-    drop_table :module_fours
-    drop_table :module_threes
-    drop_table :module_twos
-    drop_table :module_ones
-  end
-end
