a2b2703f81572cfa6411ca51685a807ec346cd47
Cliff Harris <csharris5@uh.edu>
2017-03-16 09:52:27 -0500

    html-ing Module 1

--- app/controllers/module_two_controller.rb
+++ app/controllers/module_two_controller.rb
@@ -1,5 +1,8 @@
 class ModuleTwoController < ApplicationController
   def index
     @assignments = Assignment.find_by_sql('select * from assignments inner join course_weeks on assignments.course_week_id=course_weeks.id inner join course_modules on course_weeks.course_module_id=course_modules.id where course_module_id=2')
+    @w5assignments = Assignment.where(course_week_id: 4)
+    @w6assignments = Assignment.where(course_week_id: 5)
+    @w7assignments = Assignment.where(course_week_id: 6)
   end
 end
